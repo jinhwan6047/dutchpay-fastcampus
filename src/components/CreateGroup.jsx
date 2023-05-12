3ee29ad9e22e5c9f7e780e5c6d07883284db37b0
DESKTOP-1EP1UJ8\USER <jinhwan6047@gmail.com>
2023-05-12 16:55:09 +0900

    image test

--- src/components/CreateGroup.jsx
+++ src/components/CreateGroup.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CenteredOverlayForm from "./CenteredOverlayForm";
+import testImg from "./img/testImg.jpg";
 
 export default function CreateGroup() {
   return (
     <div>
       Create Group component
+      <img src={testImg} style={{ width: "200px", height: "300px" }} />
       <CenteredOverlayForm />
     </div>
   );
