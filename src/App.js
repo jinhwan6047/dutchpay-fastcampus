1186dbf4c5309d7945b471cae3f2564eb9dbf4a3
DESKTOP-1EP1UJ8\USER <jinhwan6047@gmail.com>
2023-05-15 17:56:03 +0900

    CreateGroup

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CreateGroup from "./components/CreateGroup";
 import AddMembers from "./components/AddMembers";
 import ExpenseMain from "./components/ExpenseMain";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<CreateGroup />} />
-        <Route path="/members" element={<AddMembers />} />
-        <Route path="/expense" element={<ExpenseMain />} />
-      </Routes>
+      <RecoilRoot>
+        <Routes>
+          <Route path="/" element={<CreateGroup />} />
+          <Route path="/members" element={<AddMembers />} />
+          <Route path="/expense" element={<ExpenseMain />} />
+        </Routes>
+      </RecoilRoot>
     </BrowserRouter>
   );
 }
--- src/components/CreateGroup.jsx
+++ src/components/CreateGroup.jsx
@@ -1,11 +1,56 @@
-import React from "react";
-import CenteredOverlayForm from "./CenteredOverlayForm";
+import React, { useState } from "react";
+// import CenteredOverlayForm from "./CenteredOverlayForm";
+import { Button, Container, Form, Row } from "react-bootstrap";
+import { useRecoilState } from "recoil";
+import { groupNameState } from "../state/groupName";
 
 export default function CreateGroup() {
+  const [validated, setValidated] = useState(false);
+  const [validGroupName, setValidGroupName] = useState(false);
+  const [groupName, setGroupName] = useRecoilState(groupNameState);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity()) {
+      setValidGroupName(true);
+    } else {
+      setValidGroupName(false);
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
     <div>
-      Create Group component
-      <CenteredOverlayForm />
+      <h1>Dutch Pay</h1>
+
+      <Container>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Row>
+            <h2>먼저, 더치페이 할 그룹의 이름을 정해볼까요?</h2>
+          </Row>
+          <Row>
+            <Form.Group controlId="validationGroupName">
+              <Form.Control
+                type="text"
+                required
+                placeholder="2022 제주도 여행"
+                onChange={(e) => setGroupName(e.target.value)}
+              />
+              <Form.Control.Feedback type="invalid" data-valid={validGroupName}>
+                그룹 이름을 입력해 주세요
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <Row>
+            <Button type="submit">저장</Button>
+          </Row>
+        </Form>
+      </Container>
+
+      {/* <CenteredOverlayForm /> */}
     </div>
   );
 }
